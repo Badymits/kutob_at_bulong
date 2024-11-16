4233854a0bf38c6212f321c3d3dab7e5973ce400
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-16 22:01:15 +0800

    added temp pointer click event function

--- Kutob at Bulong/Assets/Scripts/SelectTarget.cs
+++ Kutob at Bulong/Assets/Scripts/SelectTarget.cs
@@ -4,12 +4,36 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class SelectTarget : MonoBehaviour
 {
 
     PhotonView photonView;
     ModalControl modalControl;
+    public GameObject nightSelectTargetModal;
+    public GameObject playerCardPrefab;
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log("Clicked on prefab");
+        Debug.Log("Player Name: " + playerCardPrefab.GetComponentInChildren<TMP_Text>().text);
+
+        nightSelectTargetModal.SetActive(true);
+        Debug.Log(nightSelectTargetModal);
+    }
+
+    public void CloseSelectTargetModal()
+    {
+        nightSelectTargetModal.SetActive(!gameObject.activeSelf);
+    }
+
+    public void Test()
+    {
+
+    }
+
     // Detect mouse click on this object
     void OnMouseDown()
     {
