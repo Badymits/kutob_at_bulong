a4009d2ad27e1e4ae5eb6c9e53b3fea1524481ab
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-18 01:30:50 +0800

    added start method to initialize photon component (temporary). added photon view and photon player vars for id retrieval (in progress)

--- Kutob at Bulong/Assets/Scripts/PrefabClickTest.cs
+++ Kutob at Bulong/Assets/Scripts/PrefabClickTest.cs
@@ -10,37 +10,54 @@ public class PrefabClickTest : MonoBehaviour, IPointerClickHandler
 {
     
     public Scene currentScene;
+    public int photonViewIDSelf;
+    PhotonView photonView;
     [SerializeField] private SelectTarget selectTargetScript;
-
-
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Debug.Log("Prefab clicked");
-        GameObject playerCard = gameObject;
-        Debug.Log("gameObject: " + playerCard.name);
-        Debug.Log("Player name: " + playerCard.GetComponentInChildren<TMP_Text>().text);
-        Debug.Log(playerCard.GetPhotonView().viewID);
-        Debug.Log(gameObject.name);
-
-        string sceneName = SceneManager.GetActiveScene().name;
-
-        if ( sceneName == "NightPhase")
-        {
-            selectTargetScript.OpenSelectTargetModal(playerCard.GetComponentInChildren<TMP_Text>().text, playerCard.GetPhotonView().viewID);
-        }
-    }
-
-    public void ConfirmTarget(string name)
-    {
-        
-    }
-    
+    private int photonplayerTarget;
+    private int photonplayerIDSelf;
 
     void Start()
     {
+        if (PhotonNetwork.connectedAndReady)
+        {
+            // If the player is already connected to the room, access the PhotonPlayer ID
+            Debug.Log(PhotonNetwork.player.ID);
+            photonplayerIDSelf = PhotonNetwork.player.ID;
+        }
+        photonView = GetComponent<PhotonView>();
         selectTargetScript = FindAnyObjectByType<SelectTarget>();
     }
 
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        // refers to the clicked game object. Doesn't necessarily mean it would refer to self
+        GameObject playerCard = gameObject;
+        
+        Debug.Log("Prefab clicked: " + playerCard.GetComponent<PhotonView>());
+
+        // retrieve scene for conidtional modal opening/closing
+        string sceneName = SceneManager.GetActiveScene().name;
+
+        // gets photonview component of target, NOT SELF
+        PhotonView photonViewTarget = playerCard.GetComponent<PhotonView>();
+        PhotonPlayer photonPlayerTarget = photonView.owner;
+        Debug.Log("Photon View component" + photonPlayerTarget.ID);
+        
+
+        // retrieving photon player ID's and username through the PhotonView component attached to prefab
+        int photonViewIDTarget = photonViewTarget.ownerId;
+        Debug.Log("Photon player id target: " + photonViewIDTarget);
+        Debug.Log("Photon player id self: " + photonplayerIDSelf);
+        
+        string photonViewName = playerCard.GetComponentInChildren<TMP_Text>().text;
+       
+
+        if ( sceneName == "NightPhase")
+        {
+            selectTargetScript.OpenSelectTargetModal(photonViewName, photonplayerIDSelf, photonViewIDTarget);
+        }
+    }
+    
     // Update is called once per frame
     void Update()
     {
