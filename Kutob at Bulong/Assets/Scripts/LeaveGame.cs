78917848416038eb93492019833289a4155ee960
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-18 18:14:44 +0800

    pushing changes, added new files

---
+++ Kutob at Bulong/Assets/Scripts/LeaveGame.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LeaveGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LeaveGameSession()
+    {
+        PhotonNetwork.Disconnect();
+        SceneManager.LoadScene("HomeScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
