00c4becee15eae42b134022fec2895afc3e01309
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-23 23:01:14 +0800

    added popup message on wrong code input

--- Kutob at Bulong/Assets/Scripts/JoinModalControl.cs
+++ Kutob at Bulong/Assets/Scripts/JoinModalControl.cs
@@ -9,7 +9,14 @@ using UnityEngine.UI;
 public class JoinModalControl : MonoBehaviour
 {
     public TMP_InputField inputField;
+    public GameObject message;
     public GameObject modal;
+    private PopupMessage popupMessage;
+
+    void Start()
+    {
+        popupMessage = FindAnyObjectByType<PopupMessage>();
+    }
     // Start is called before the first frame update
     public void OpenModal()
     {
@@ -27,5 +34,42 @@ public class JoinModalControl : MonoBehaviour
         {
             PhotonNetwork.JoinRoom(inputField.text.ToUpper());
         }
+        else
+        {
+            ShowErrorMessage();
+        }
+    }
+
+    private void ShowErrorMessage()
+    {
+        message.SetActive(true);
+        CloseMessage();
+    }
+
+    // added delay to not close immediately
+    public void CloseMessage()
+    {
+        if (message != null && message.activeInHierarchy)
+        {
+            StartCoroutine(CloseMessageObject());
+        }
+        return;
+    }
+
+    IEnumerator CloseMessageObject()
+    {
+        yield return new WaitForSeconds(5f);
+
+        message.SetActive(false);
+    }
+
+    // This is called when the room join fails in Photon 1.x
+    public void OnPhotonJoinRoomFailed(object[] codeAndMsg)
+    {
+        // codeAndMsg contains the error code and the error message
+        short errorCode = (short)codeAndMsg[0];
+        string errorMsg = (string)codeAndMsg[1];
+
+        
     }
 }
