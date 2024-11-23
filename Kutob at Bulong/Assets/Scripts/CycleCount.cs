fc82bba7d996fe4ae36da3d20383bd5d3a98a962
Elijah Cajucom <elijah.cajucom@gmail.com>
2024-11-23 22:59:56 +0800

    added condition to update night/day count through RPC call to sync users

--- Kutob at Bulong/Assets/Scripts/CycleCount.cs
+++ Kutob at Bulong/Assets/Scripts/CycleCount.cs
@@ -9,28 +9,72 @@ public class CycleCount : MonoBehaviour
     public TMP_Text count;
     private bool isTransitioning = false;
     public float roleSceneTimer = 5f;
-    int night_counter = 0;
-    int day_counter = 0;
+    int night_counter;
+    int day_counter;
     public PhotonPlayer photonPlayer;
+    public PhotonView photonView;
 
     void Start()
     {
-        photonPlayer = PhotonNetwork.player;
-        Scene currentScene = SceneManager.GetActiveScene();
-        Debug.Log(currentScene.name.ToString());
-        StartCoroutine(TimerToNextScene(currentScene.name.ToString()));
+        // Get initial counters from room custom properties
+        night_counter = (int)PhotonNetwork.room.CustomProperties["Night_Count"];
+        day_counter = (int)PhotonNetwork.room.CustomProperties["Day_Count"];
 
-        if (name == "NightTransition")
+        
+        photonView = FindAnyObjectByType<PhotonView>();
+
+        // Update the UI with current values
+        Scene currentScene = SceneManager.GetActiveScene();
+        Debug.Log(currentScene.name);
+
+        // Set custom properties if it's the master client
+        if (PhotonNetwork.isMasterClient)
+        {
+            photonView.RPC("SetNightOrDayCount", PhotonTargets.All, currentScene.name);
+        }
+    }
+
+    [PunRPC]
+    void SetNightOrDayCount(string sceneName)
+    {
+        // Increment the counters based on the scene
+        if (sceneName == "NightTransition")
         {
             night_counter++;
-            count.text = night_counter.ToString();
+            // Update room custom properties
+            ExitGames.Client.Photon.Hashtable newProperties = new ExitGames.Client.Photon.Hashtable();
+            newProperties["Night_Count"] = night_counter;
+            PhotonNetwork.room.SetCustomProperties(newProperties);
         }
         else
         {
             day_counter++;
-            count.text = day_counter.ToString();
+            ExitGames.Client.Photon.Hashtable newProperties = new ExitGames.Client.Photon.Hashtable();
+            newProperties["Day_Count"] = day_counter;
+            PhotonNetwork.room.SetCustomProperties(newProperties);
         }
 
+        // Update the UI locally
+        count.text = (sceneName == "NightTransition") ? night_counter.ToString() : day_counter.ToString();
+
+        // Start timer for next scene transition
+        StartCoroutine(TimerToNextScene(sceneName));
+    }
+
+    // Listen for custom property changes (will be called on all clients)
+    void OnPhotonCustomRoomPropertiesChanged(ExitGames.Client.Photon.Hashtable propertiesThatChanged)
+    {
+        if (propertiesThatChanged.ContainsKey("Night_Count"))
+        {
+            night_counter = (int)propertiesThatChanged["Night_Count"];
+            count.text = night_counter.ToString();
+        }
+
+        if (propertiesThatChanged.ContainsKey("Day_Count"))
+        {
+            day_counter = (int)propertiesThatChanged["Day_Count"];
+            count.text = day_counter.ToString();
+        }
     }
 
     private IEnumerator TimerToNextScene(string name)
@@ -40,17 +84,16 @@ public class CycleCount : MonoBehaviour
         // Wait for the specified time in seconds
         yield return new WaitForSeconds(roleSceneTimer);
 
-        // After the timer expires, load the introduction scene
+        // After the timer expires, load the appropriate scene
         if (!isTransitioning)
         {
             isTransitioning = true;
-            Debug.Log("Timer expired. Transitioning to the Introduction Scene.");
+            Debug.Log("Timer expired. Transitioning to the next scene.");
 
-            // Transition to the introduction scene for all players
+            // Transition to the appropriate phase (Night/Day)
             if (name == "NightTransition")
             {
                 Increment("Night");
-
             }
             else
             {
@@ -70,29 +113,40 @@ public class CycleCount : MonoBehaviour
         {
             switchtoDayPhase();
         }
-
     }
-
-
 
     void switchToNightPhase()
     {
-
-        PhotonNetwork.LoadLevel("NightPhase");
+        // Ensure the scene transition happens for all players
+        photonView.RPC("TransitionToNight", PhotonTargets.All);
     }
 
     void switchtoDayPhase()
     {
+        // Ensure the scene transition happens for all players
+        photonView.RPC("TransitionToDay", PhotonTargets.All);
+    }
+
+    [PunRPC]
+    void TransitionToNight()
+    {
+        PhotonNetwork.LoadLevel("NightPhase");
+    }
+
+    [PunRPC]
+    void TransitionToDay()
+    {
         Debug.Log("local player: " + photonPlayer);
+        photonPlayer = PhotonNetwork.player;  // Using PhotonPlayer in PUN 1
         if (!(bool)photonPlayer.CustomProperties["isAlive"])
         {
-            //PhotonNetwork.LoadLevel("EliminatedScene");
-            SceneManager.LoadScene("EliminatedScene");
+            // For players who are eliminated, load the eliminated scene
+            PhotonNetwork.LoadLevel("EliminatedScene");
         }
         else
         {
+            // For alive players, load the discussion phase scene
             PhotonNetwork.LoadLevel("Discussion Phase");
         }
-        
     }
 }
